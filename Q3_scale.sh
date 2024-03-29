cdfadd6daeaa45a8f47ba2835bc056838b4d7d9f
Ubuntu <ubuntu@ip-172-31-68-252.ec2.internal>
2024-03-29 20:09:04 +0000

    Scale

--- Q3_scale.sh
+++ Q3_scale.sh
@@ -1,11 +1,5 @@
-python scripts/run.py --mode=eval --agent_cfg yolo.yaml --scenario_cfg object_detection_stopsign.yaml --patch 'stopsign_q3_scale50' --exp_name scale_50_yolo --num_scenario 4 --save_video
-python scripts/run.py --mode=eval --agent_cfg yolo.yaml --scenario_cfg object_detection_stopsign.yaml --patch 'stopsign_q3_scale100' --exp_name scale_100_yolo --num_scenario 4 --save_video
-python scripts/run.py --mode=eval --agent_cfg yolo.yaml --scenario_cfg object_detection_stopsign.yaml --patch 'stopsign_q3_scale150' --exp_name scale_150_yolo --num_scenario 4 --save_video
-python scripts/run.py --mode=eval --agent_cfg yolo.yaml --scenario_cfg object_detection_stopsign.yaml --patch 'stopsign_q3_scale200' --exp_name scale_200_yolo --num_scenario 4 --save_video
-python scripts/run.py --mode=eval --agent_cfg yolo.yaml --scenario_cfg object_detection_stopsign.yaml --patch 'stopsign_q3_scale250' --exp_name scale_250_yolo --num_scenario 4 --save_video
-
-python scripts/run.py --mode=eval --agent_cfg faster_rcnn.yaml --scenario_cfg object_detection_stopsign.yaml --patch 'stopsign_q3_scale50' --exp_name scale_50_frcnn --num_scenario 4 --save_video
-python scripts/run.py --mode=eval --agent_cfg faster_rcnn.yaml --scenario_cfg object_detection_stopsign.yaml --patch 'stopsign_q3_scale100' --exp_name scale_100_frcnn --num_scenario 4 --save_video
-python scripts/run.py --mode=eval --agent_cfg faster_rcnn.yaml --scenario_cfg object_detection_stopsign.yaml --patch 'stopsign_q3_scale150' --exp_name scale_150_frcnn --num_scenario 4 --save_video
-python scripts/run.py --mode=eval --agent_cfg faster_rcnn.yaml --scenario_cfg object_detection_stopsign.yaml --patch 'stopsign_q3_scale200' --exp_name scale_200_frcnn --num_scenario 4 --save_video
-python scripts/run.py --mode=eval --agent_cfg faster_rcnn.yaml --scenario_cfg object_detection_stopsign.yaml --patch 'stopsign_q3_scale250' --exp_name scale_250_frcnn --num_scenario 4 --save_video
+python scripts/run.py --mode=eval --agent_cfg faster_rcnn.yaml --scenario_cfg object_detection_stopsign.yaml --patch 'stopsign_q3_scale50' --exp_name scale_50_frcnn --num_scenario 4
+python scripts/run.py --mode=eval --agent_cfg faster_rcnn.yaml --scenario_cfg object_detection_stopsign.yaml --patch 'stopsign_q3_scale100' --exp_name scale_100_frcnn --num_scenario 4 
+python scripts/run.py --mode=eval --agent_cfg faster_rcnn.yaml --scenario_cfg object_detection_stopsign.yaml --patch 'stopsign_q3_scale150' --exp_name scale_150_frcnn --num_scenario 4
+python scripts/run.py --mode=eval --agent_cfg faster_rcnn.yaml --scenario_cfg object_detection_stopsign.yaml --patch 'stopsign_q3_scale200' --exp_name scale_200_frcnn --num_scenario 4
+python scripts/run.py --mode=eval --agent_cfg faster_rcnn.yaml --scenario_cfg object_detection_stopsign.yaml --patch 'stopsign_q3_scale250' --exp_name scale_250_frcnn --num_scenario 4
--- scripts/run.py
+++ scripts/run.py
@@ -42,7 +42,7 @@ if __name__ == '__main__':
     parser.add_argument('--tm_port', type=int, default=8000, help='traffic manager port')
     parser.add_argument('--fixed_delta_seconds', type=float, default=0.1)
     parser.add_argument('--max_episode_step', type=int, default=300)
-    parser.add_argument('--patch', type=str, default='stopsign.jpg')
+    parser.add_argument('--patch', type=str, default='stopsign')
     args = parser.parse_args()
     args_dict = vars(args)
 
@@ -59,7 +59,7 @@ if __name__ == '__main__':
     scenario_config_path = osp.join(args.ROOT_DIR, 'safebench/scenario/config', args.scenario_cfg)
     scenario_config = load_config(scenario_config_path)
     scenario_config['texture_dir'] = f'safebench/scenario/scenario_data/template_od/{args.patch}.jpg'
-
+    print(f"Patch: {args.patch}")
     # main entry with a selected mode
     agent_config.update(args_dict)
     scenario_config.update(args_dict)
